7693944dfc819ea880df0ffa2067cf78dea51c47
batistadaniel <109699716+batistadaniel@users.noreply.github.com>
2024-06-26 10:24:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Atividade - PHP 2 - parte 1</title>
+</head>
+<body>
+    <main>
+        <form action="resultado.php" method="post" id="formulario" class="formulario">
+
+            <label for="velocidade">Digite uma velocidade: </label>
+            <input type="text" id="velocidade" name="velocidade">
+
+            <input type="submit" value="Verificar">
+
+        </form>
+    </main>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,43 @@
+<!-- <?php
+    $velocidade = $_POST["velocidade"];  
+    $velocidadeMedia = 60;
+    $velocidadeReduza = 80;
+
+    if ($velocidade == $velocidadeMedia):
+        echo '<p style="color: green; font-size:42px;">Prossiga<p>';
+
+    elseif ($velocidade == $velocidadeReduza):
+        echo '<p style="color: yellow; font-size:42px">Reduza a velocidade<p>';
+    elseif ($velocidade > $velocidadeReduza):
+        echo '<p style="color: red; font-size:42px">Voce foi multado<p>';
+    endif;    
+?> -->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Dados Recebidos</title>
+</head>
+<body>
+    <!-- div com nome da mesma classe do formulario para exdibir as informacoes no "mesmo" layout. xD--->
+    <div class="formulario">
+        <?php
+            $velocidade = @$_POST["velocidade"];  
+            $velocidadeMedia = 60;
+            $velocidadeReduza = 80;
+
+            if ($velocidade <= $velocidadeMedia):
+                echo '<p style="color: green; font-size:48px;">Prossiga<p>';
+
+            elseif ($velocidade > $velocidadeMedia && $velocidade <= $velocidadeReduza):
+                echo '<p style="color: yellow; font-size:48px">Reduza a velocidade<p>';
+            elseif ($velocidade > $velocidadeReduza):
+                echo '<p style="color: red; font-size:48px">Voce foi multado<p>';
+            endif;    
+        ?>
+    </div>
+</body>
+</html>
